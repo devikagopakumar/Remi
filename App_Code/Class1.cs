e42944fe8921b3b67a005eaf1710f334dde8d035
devikagopakumar <todevikagopakumar@gmail.com>
2018-12-20 12:44:36 +0530

    first commit

---
+++ App_Code/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+    SqlConnection con;
+    public Class1();
+
+	public Class1()
+	{
+
+        con = new SqlConnection("Data Source=USER-PC; Initial Catalog=tour;Integrated Security=SSPI");
+        con.Open();
+
+	}
+    
+    public void insert(String s)
+    {
+        SqlCommand cmd;
+        cmd = new SqlCommand(s, con);
+        cmd.ExecuteNonQuery();
+       
+    }
+    public SqlDataReader select(String s11)
+    {
+        SqlCommand cmd;
+        SqlDataReader dr; 
+        cmd = new SqlCommand(s11, con);
+        dr = cmd.ExecuteReader();
+        return dr;
+    }
+    public void update(String s12)
+    {
+        SqlCommand cmd;
+        cmd = new SqlCommand(s12, con);
+        cmd.ExecuteNonQuery();
+    }
+    public void delete(String s00)
+    {
+        SqlCommand cmd;
+        cmd = new SqlCommand(s00, con);
+        cmd.ExecuteNonQuery();
+    }
+}
+}
---
+++ MasterPage.master.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    Class1 db;
+    SqlConnection con;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+        con = new SqlConnection("Data Source=USER-PC; Initial Catalog=tour;Integrated Security=SSPI");
+        con.Open();
+
+    }
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        TextBox1.Text = Calendar1.SelectedDate.ToString();
+    }
+   
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        TextBox3.Text = DropDownList1.Text + ":" + DropDownList2.Text + " " + DropDownList3.Text;
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string s = "insert into reminder values('"+TextBox1.Text+"','"+TextBox3.Text+"','"+TextBox2.Text+"')";
+        Response.Write("<script> alert('Reminder Is Setted Properly')");
+        db.insert(s);
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string s12 = "update reminder set reminder_date='"+TextBox1.Text.ToString()+"'";
+        db.update(s12);
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        string s00 = "delete from reminder where reminder_date='" + TextBox1.Text.ToString() + "' ";
+        db.delete(s00);
+    }
+}
